bd44cc7b8c426ceedc81351c93998f58f187f685
josousa <joao.agostinhodesousa@hest.ethz.ch>
2021-01-30 22:17:18 +0100

    Added all the config files to the git repository

---
+++ nme_startup.sh
@@ -0,0 +1,11 @@
+## Define location of user modules
+export MODULEPATH=/cluster/work/nme/software/modules:$MODULEPATH
+
+## Define Variables to quick access NME folders
+export WORK=/cluster/work/nme
+export PUBLIC=/nfs/nas12.ethz.ch/fs1201/green_groups_nme_public
+export LABRADOR=/nfs/green-nme.ethz.ch/data/labrador
+export GENOMES=/cluster/work/nme/genomes
+
+## LMOD variables
+export LMOD_IGNORE_CACHE=1
